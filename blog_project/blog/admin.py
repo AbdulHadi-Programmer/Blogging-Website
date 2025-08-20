f0611d253d53f77d1aa025f9c0a699442368a745
AbdulHadi-Programmer <abdulhadistmstudent@gmail.com>
2025-08-20 20:48:51 +0500

    make changes to admin panel

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -3,7 +3,7 @@ from .models import *
 # Register your models here.
 
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'summary')
+    list_display = ('title', 'summary', 'is_published')
     list_editable = ('is_published', )
 
 admin.site.register(BlogPost, BlogPostAdmin)
